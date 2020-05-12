f59ffcc78a1df9de6889ca6c03ff7e24de91f48c
mtisel <maja.tisel@gmail.com>
2020-05-12 20:57:11 +0200

    Updates

---
+++ precache-manifest.93885faf10b474940dd899697219d840.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb8b6ecf9e19bf8f0f4357bf6fc334f",
+    "url": "/mtisel.github.io/portfolio/index.html"
+  },
+  {
+    "revision": "8afd5694dc3e0b625689",
+    "url": "/mtisel.github.io/portfolio/static/css/main.00a90d94.chunk.css"
+  },
+  {
+    "revision": "83c96f8d45fbe8ed17c3",
+    "url": "/mtisel.github.io/portfolio/static/js/2.adb5e262.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mtisel.github.io/portfolio/static/js/2.adb5e262.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8afd5694dc3e0b625689",
+    "url": "/mtisel.github.io/portfolio/static/js/main.81a523a8.chunk.js"
+  },
+  {
+    "revision": "b8bccbb574ec251790f6",
+    "url": "/mtisel.github.io/portfolio/static/js/runtime-main.0010008b.js"
+  },
+  {
+    "revision": "4a24899e94d8236f671c1090cd9e068c",
+    "url": "/mtisel.github.io/portfolio/static/media/Canela-Regular.4a24899e.ttf"
+  }
+]);
